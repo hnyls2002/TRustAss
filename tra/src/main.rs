bc607002d714a6578d4efef3a4024f925bcb6445
hnyls2002 <hnyls2002@gmail.com>
2023-07-03 21:45:26 +0800

    ✨ feat(inotify): a demo file watcher

--- tra/src/main.rs
+++ tra/src/main.rs
@@ -1,10 +1,13 @@
 use std::thread;
 
+use inotify::{Inotify, WatchMask};
+
 mod client;
 mod debugger;
+mod fs;
 mod server;
 
-fn main() {
+pub fn test_socket() -> std::io::Result<()> {
     let server_thread = thread::spawn(|| {
         server::start_server().expect("Server failed");
     });
@@ -18,4 +21,33 @@ fn main() {
 
     server_thread.join().expect("Server thread panicked");
     client_thread.join().expect("Client thread panicked");
+
+    Ok(())
+}
+
+pub fn file_watch_test(dir_path: &str) {
+    let mut inotify = Inotify::init().expect("Failed to initialize inotify");
+    let path = std::path::Path::new(dir_path);
+    std::fs::create_dir_all(path).unwrap();
+
+    debug!("All events can be watched {:?}", WatchMask::ALL_EVENTS);
+
+    inotify.watches().add(path, WatchMask::ALL_EVENTS).unwrap();
+
+    let mut buffer = [0; 1024];
+
+    loop {
+        debug!("Waiting for events");
+        let events = inotify.read_events_blocking(buffer.as_mut()).unwrap();
+
+        for event in events {
+            println!("{:?}", event);
+        }
+    }
+}
+
+fn main() {
+    // test_socket().unwrap();
+
+    file_watch_test("tmp/");
 }
